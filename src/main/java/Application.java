aeb4894dd67a690b64ab838cd7f82a2eaade2c13
aatwi <@twi20185>
2019-09-13 13:11:59 +0300

    - Add header text to the Java files

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,14 @@
-import com.murex.tbw.MainRepository;
+/*******************************************************************************
+ *
+ * Copyright (c) {2003-2019} Murex S.A.S. and its affiliates.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+
 import com.murex.tbw.report.ReportGenerator;
-import com.murex.tbw.storage.Repository;
 
 public class Application {
     public static void main(String[] args) {
