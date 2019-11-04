4f9c3061e7979d05aed4d36b7dbf92e42d5b4118
aatwi <@twi20185>
2019-11-04 16:33:36 +0200

    - Add header text to the C++ files

--- src/main/cpp/lib/BooksOrder.cpp
+++ src/main/cpp/lib/BooksOrder.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ *
+ * Copyright (c) {2003-2019} Murex S.A.S. and its affiliates.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+
 #include "client/BooksOrder.h"
 #include "MainRepository.h"
 #include "storage/Repository.h"
