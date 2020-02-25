90eb1c4b47e24dc39016ab91f43caeacef8ce730
aatwi <@twi20185>
2020-02-25 13:34:44 +0200

    Change the license text in each of the Java & C++ files

--- src/main/cpp/lib/domain/country/Country.h
+++ src/main/cpp/lib/domain/country/Country.h
@@ -2,9 +2,9 @@
  *
  * Copyright (c) {2003-2019} Murex S.A.S. and its affiliates.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the MIT License
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://opensource.org/licenses/MIT
  *
  *******************************************************************************/
 
