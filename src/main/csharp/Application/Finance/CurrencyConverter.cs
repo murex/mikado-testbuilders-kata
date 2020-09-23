ccac5d62a7fe6c9385b31f4afbae77109a713930
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-09-23 21:34:21 +0200

    Add FInance

---
+++ src/main/csharp/Application/Finance/CurrencyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Application.Domain.Country;
+using static Application.Domain.Country.Currency;
+
+namespace Application.Finance
+{
+    public sealed class CurrencyConverter
+    {
+        private static readonly IReadOnlyDictionary<Currency, double> ExchangeRate =
+            new Dictionary<Currency, double>
+            {
+                {UsDollar, 1.0},
+                {Euro, 1.14},
+                {PoundSterling, 1.27},
+                {Renminbi, 0.15},
+                {Yen, 0.0093},
+                {AustralianDollar, 0.7}
+            };
+
+        public static double FromUsd(double input, Currency currency) => input / ExchangeRate[currency];
+
+        public static double ToUsd(double input, Currency currency) => input * ExchangeRate[currency];
+    }
+}
---
+++ src/main/csharp/Application/Finance/TaxRule.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Application.Domain.Book;
+using Application.Domain.Country;
+using static Application.Domain.Country.Language;
+
+namespace Application.Finance
+{
+    public static class TaxRule
+    {
+        private static readonly IReadOnlyDictionary<string, double> TaxRates = new Dictionary<string, double>
+        {
+            {"USA", 1.15},
+            {"France", 1.25},
+            {"UK", 1.20d},
+            {"Spain", 1.10d},
+            {"China", 1.35d},
+            {"Japan", 1.30d},
+            {"Australia", 1.13d},
+            {"Germany", 1.22d},
+        };
+
+        public static double GetApplicableRate(Country invoiceCountry, IBook book)
+        {
+            if (invoiceCountry.Name.Equals("Germany"))
+                if (book.Author.Nationality.Name.Equals("Germany"))
+                    return 1.05;
+
+            if (invoiceCountry.Name.Equals("USA"))
+                if (book is Novel)
+                    return GetTaxRate(invoiceCountry.Name) * 0.98;
+
+            if (invoiceCountry.Name.Equals("UK"))
+                if (book is Novel)
+                    return GetTaxRate(invoiceCountry.Name) * 0.93;
+
+
+            if (invoiceCountry.Name.Equals("China"))
+                if (!book.Language.Equals(Mandarin))
+                    return 1;
+
+            if (invoiceCountry.Name.Equals("Spain"))
+                if (!book.Language.Equals(Spanish))
+                    return 1;
+
+            return GetTaxRate(invoiceCountry.Name);
+        }
+
+        private static double GetTaxRate(string country) => TaxRates[country];
+    }
+}
