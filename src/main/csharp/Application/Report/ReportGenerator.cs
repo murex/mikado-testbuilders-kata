1cc2d7769ef0e9f500e8dbe915bb4ec4bcd0b560
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-09-23 21:46:41 +0200

    Add ReportGenerator

---
+++ src/main/csharp/Application/Report/ReportGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Application.Purchase;
+using Application.Storage;
+
+namespace Application.Report
+{
+    public class ReportGenerator
+    {
+        private readonly IRepository _repository = MainRepository.ConfiguredRepository;
+
+        public double GetTotalAmount()
+        {
+            var invoices = _repository.GetInvoiceMap().Values;
+            var totalAmount = invoices.Sum(invoice => invoice.ComputeTotalAmount());
+            return totalAmount;
+        }
+
+        public int GetTotalSoldBooks()
+        {
+            var invoices = _repository.GetInvoiceMap().Values;
+            var totalSoldBooks = invoices
+                .Sum(invoice => invoice.PurchasedBooks.Sum(purchasedBook => purchasedBook.Quantity));
+
+            return totalSoldBooks;
+        }
+
+        public long GetNumberOfIssuedInvoices() => _repository.GetInvoiceMap().Count;
+    }
+}
