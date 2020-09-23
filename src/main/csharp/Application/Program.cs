88b0e9bf5fd071e8309650d1dd4131ce1571e53b
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-09-23 16:21:41 +0200

    Add empty Application and Tests C# projects

---
+++ src/main/csharp/Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/test/csharp/Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestsCanRun() => Assert.True(true);
+    }
+}
