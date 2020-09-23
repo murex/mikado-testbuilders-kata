13dce64dc4283b518e977ee68f8ed71d3d6b7f50
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-09-23 21:08:04 +0200

    Add dummy JsonRepository

--- src/main/csharp/Application/MainRepository.cs
+++ src/main/csharp/Application/MainRepository.cs
@@ -4,6 +4,22 @@ namespace Application
 {
     public sealed class MainRepository
     {
-        public static IRepository ConfiguredRepository { get; }
+        public static IRepository ConfiguredRepository { get; set; } = new JsonRepository();
+
+        /**
+         * Working effectively with Legacy Code
+         * https://www.goodreads.com/book/show/44919.Working_Effectively_with_Legacy_Code
+         */
+        // TESTING ONLY
+        public void Override(IRepository newRepository)
+        {
+            ConfiguredRepository = newRepository;
+        }
+
+        // TESTING ONLY
+        public void Reset()
+        {
+            ConfiguredRepository = new JsonRepository();
+        }
     }
 }
---
+++ src/main/csharp/Application/Storage/JsonRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Application.Purchase;
+
+namespace Application.Storage
+{
+    public sealed class JsonRepository : IRepository
+    {
+        public void AddInvoice(Invoice invoice)
+        {
+            
+        }
+
+        public Dictionary<int, Invoice> GetInvoiceMap()
+        {
+            return new Dictionary<int, Invoice>();
+        }
+    }
+}
