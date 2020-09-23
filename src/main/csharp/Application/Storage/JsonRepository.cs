502f1b49f97a9d964315abfda722bf909d9c7837
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-09-23 23:46:10 +0200

    Finalize JsonRepository implementation

--- src/main/csharp/Application/Storage/JsonRepository.cs
+++ src/main/csharp/Application/Storage/JsonRepository.cs
@@ -16,39 +16,63 @@ namespace Application.Storage
 {
     public sealed class JsonRepository : IRepository
     {
-        public Record[] Records { get; private set; }
+        public Dictionary<int, Invoice> Invoices { get; }
 
         public JsonRepository()
         {
+            Invoices = new Dictionary<int, Invoice>();
             LoadJsonData();
         }
 
         private void LoadJsonData()
         {
-            Records = JsonSerializer.Deserialize<Record[]>(
+            var records = JsonSerializer.Deserialize<Record[]>(
                 File.ReadAllText("../../../../../../main/resources/repository.json"),
                 new JsonSerializerOptions()
                 {
                     PropertyNameCaseInsensitive = true,
                     Converters = {new JsonStringEnumConverter()}
                 });
+
+            foreach (var record in records)
+            {
+                var invoice = new Invoice(
+                    record.Id,
+                    record.Client,
+                    record.Country.ToDomain());
+
+                var purchasedBooks = record.BooksInBasket
+                    .Select(b =>
+                    {
+                        IBook book = b.Category.HasValue
+                            ? (IBook) new EducationalBook(b.Name, b.Price, b.Author.ToDomain(), b.Language,
+                                b.Category.Value)
+                            : (IBook) new Novel(b.Name, b.Price, b.Author.ToDomain(), b.Language, b._Genre);
+
+
+                        return new PurchasedBook(book, b.Quantity);
+                    })
+                    .ToList();
+
+                invoice.AddPurchasedBooks(purchasedBooks);
+
+                AddInvoice(invoice);
+            }
         }
 
         public void AddInvoice(Invoice invoice)
         {
+            Invoices.Add(invoice.Id, invoice);
         }
 
-        public Dictionary<int, Invoice> GetInvoiceMap()
-        {
-            return new Dictionary<int, Invoice>();
-        }
+        public Dictionary<int, Invoice> GetInvoiceMap() => Invoices;
 
         public class Record
         {
             public int Id { get; set; }
             public string Client { get; set; }
             public Country Country { get; set; }
-            public BooksInBasket[] BooksInBasket { get; set; }
+            public BookInBasket[] BooksInBasket { get; set; }
         }
 
         public class Country
@@ -58,15 +82,17 @@ namespace Application.Storage
             public string Currency { get; set; }
 
             public Currency _Currency => Convert.ToCurrency(Currency);
+
+            public Domain.Country.Country ToDomain() => new Domain.Country.Country(Name, _Currency, Language);
         }
 
-        public class BooksInBasket
+        public class BookInBasket
         {
             public string Name { get; set; }
             public float Price { get; set; }
             public int Quantity { get; set; }
             public Language Language { get; set; }
-            public Category Category { get; set; }
+            public Category? Category { get; set; }
             public Author Author { get; set; }
             public string[] Genre { get; set; } = new string[0];
 
@@ -77,6 +103,8 @@ namespace Application.Storage
         {
             public string Name { get; set; }
             public Nationality Nationality { get; set; }
+
+            public Domain.Book.Author ToDomain() => new Domain.Book.Author(Name, Nationality.ToCountry());
         }
 
         public class Nationality
@@ -85,6 +113,8 @@ namespace Application.Storage
             public Language Language { get; set; }
             public string Currency { get; set; }
             public Currency _Currency => Convert.ToCurrency(Currency);
+
+            public Domain.Country.Country ToCountry() => new Domain.Country.Country(Name, _Currency, Language);
         }
 
         public class Convert
