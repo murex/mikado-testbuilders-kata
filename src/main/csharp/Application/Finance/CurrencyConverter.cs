36150cad51fcbdebb775b9ecf442eede5e16825b
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-10-07 16:54:12 +0200

    Enforce immutability

--- src/main/csharp/Application/Finance/CurrencyConverter.cs
+++ src/main/csharp/Application/Finance/CurrencyConverter.cs
@@ -8,15 +8,16 @@ namespace Application.Finance
     public sealed class CurrencyConverter
     {
         private static readonly IReadOnlyDictionary<Currency, double> ExchangeRate =
-            new Dictionary<Currency, double>
-            {
-                {UsDollar, 1.0},
-                {Euro, 1.14},
-                {PoundSterling, 1.27},
-                {Renminbi, 0.15},
-                {Yen, 0.0093},
-                {AustralianDollar, 0.7}
-            };
+            new ReadOnlyDictionary<Currency, double>(
+                new Dictionary<Currency, double>
+                {
+                    {UsDollar, 1.0},
+                    {Euro, 1.14},
+                    {PoundSterling, 1.27},
+                    {Renminbi, 0.15},
+                    {Yen, 0.0093},
+                    {AustralianDollar, 0.7}
+                });
 
         public static double FromUsd(double input, Currency currency) => input / ExchangeRate[currency];
 
