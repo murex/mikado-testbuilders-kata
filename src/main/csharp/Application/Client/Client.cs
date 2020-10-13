5024a7ab0f891500467b07b81e7579c5a7c6f369
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-10-13 17:26:36 +0200

    Add copyright
    Powershell command-line :
    Get-ChildItem *.cs -Recurse | %{ Set-Content -Path $_ -value ("$(Get-Content ..\..\..\COPYRIGHT.txt -Raw)$(Get-Content $_ -Raw)") }

--- src/main/csharp/Application/Client/Client.cs
+++ src/main/csharp/Application/Client/Client.cs
@@ -1,4 +1,14 @@
-﻿using System;
+/*******************************************************************************
+ *
+ * Copyright (c) {2003-2019} Murex S.A.S. and its affiliates.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * https://opensource.org/licenses/MIT
+ *
+ *******************************************************************************/
+
+using System;
 using Application.Domain.Country;
 
 namespace Application.Client
--- src/main/csharp/Application/Client/IOrder.cs
+++ src/main/csharp/Application/Client/IOrder.cs
@@ -1,4 +1,14 @@
-﻿using Application.Domain.Book;
+/*******************************************************************************
+ *
+ * Copyright (c) {2003-2019} Murex S.A.S. and its affiliates.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * https://opensource.org/licenses/MIT
+ *
+ *******************************************************************************/
+
+using Application.Domain.Book;
 using Application.Purchase;
 
 namespace Application.Client
