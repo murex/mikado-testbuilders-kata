727e7abac12a3d87b644990d8b116be00beadd10
Christophe Cadilhac <christophe.cadilhac@cdbdx.biz>
2020-10-30 17:07:55 +0100

    Make MainRepository static so that Override() and Reset() are actually usable

--- src/csharp/Application/MainRepository.cs
+++ src/csharp/Application/MainRepository.cs
@@ -12,22 +12,22 @@ using Application.Storage;
 
 namespace Application
 {
-    public sealed class MainRepository
+    public static class MainRepository
     {
-        public static IRepository ConfiguredRepository { get; set; } = new JsonRepository();
+        public static IRepository ConfiguredRepository { get; private set; } = new JsonRepository();
 
         /**
          * Working effectively with Legacy Code
          * https://www.goodreads.com/book/show/44919.Working_Effectively_with_Legacy_Code
          */
         // TESTING ONLY
-        public void Override(IRepository newRepository)
+        public static void Override(IRepository newRepository)
         {
             ConfiguredRepository = newRepository;
         }
 
         // TESTING ONLY
-        public void Reset()
+        public static void Reset()
         {
             ConfiguredRepository = new JsonRepository();
         }
