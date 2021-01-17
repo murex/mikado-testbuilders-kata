814262d1a5b3b29fdb782ce4e5feb21c0f278fea
Philippe Bourgau <philippe.bourgau@gmail.com>
2021-01-17 07:20:16 +0100

    Refactor Country to use readonly fields
    To use more idiomatic TypeScript
    - replace private fields + public getters to readonly fields
    - update TODO

--- typescript/src/com/murex/tbw/domain/country/Country.ts
+++ typescript/src/com/murex/tbw/domain/country/Country.ts
@@ -2,18 +2,14 @@ import {Currency} from "./Currency"
 import {Language} from "./Language"
 
 export class Country {
-    private _name:String;
-    private _currency:Currency;
-    private _language:Language;
+    readonly name:String;
+    readonly currency:Currency;
+    readonly language:Language;
 
     public constructor(name: String, currency: Currency, language: Language)
     {
-        this._name = name;
-        this._currency = currency;
-        this._language = language;
+        this.name = name;
+        this.currency = currency;
+        this.language = language;
     }
-
-    public get name(): String { return this._name; }
-    public get currency(): Currency { return this._currency; }
-    public get language(): Language { return this._language; }
 }
