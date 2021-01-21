891570b231962bcf69273a8a32c9b4e0601633ed
aatwi <ahmad.atwi@murex.com>
2021-01-21 15:41:55 +0100

    Inorder to fix the Rounding Issue in Java
    1. Add a function in ReportGenerator to round the value to 0.00
    2. Update the values in the MD file.

--- java/src/main/java/com/murex/tbw/report/ReportGenerator.java
+++ java/src/main/java/com/murex/tbw/report/ReportGenerator.java
@@ -11,16 +11,17 @@
 package com.murex.tbw.report;
 
 import com.murex.tbw.MainRepository;
-import com.murex.tbw.finance.CurrencyConverter;
 import com.murex.tbw.purchase.Invoice;
 import com.murex.tbw.purchase.PurchasedBook;
 import com.murex.tbw.storage.Repository;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 
 public class ReportGenerator {
 
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     private final Repository repository = MainRepository.configuredRepository();
 
     public double getTotalAmount() {
@@ -31,7 +32,11 @@ public class ReportGenerator {
 //            TODO Uncomment to fix the bug in ReportGenerator
 //            totalAmount += CurrencyConverter.toUSD(invoice.computeTotalAmount(), invoice.getCountry().getCurrency());
         }
-        return totalAmount;
+        return getRoundedAmount(totalAmount);
+    }
+
+    private double getRoundedAmount(double totalAmount) {
+        return Math.round(totalAmount * 100.0) / 100.0;
     }
 
     public int getTotalSoldBooks() {
