fab13d66ac89455a0ba9479666b4d8d4b8fbc4bc
aatwi <ahmad.atwi@murex.com>
2021-03-24 15:19:03 +0100

    Adding a compiling version of the CPP solution

--- cpp/tests/Tests.cpp
+++ cpp/tests/Tests.cpp
@@ -1,5 +1,13 @@
 #include "gtest/gtest.h"
 #include "report/ReportGenerator.h"
+#include <purchase/Invoice.h>
+#include <domain/country/Country.h>
+#include <domain/country/Currency.h>
+#include <domain/country/Language.h>
+#include <domain/book/Book.h>
+#include <domain/book/Novel.h>
+#include <purchase/PurchasedBook.h>
+
 
 using namespace std;
 
@@ -54,9 +62,16 @@ TEST(REPORT_GENERATOR, DISABLED_Mikado_Method_And_Test_Data_Builders_Constraint_
 #pragma region Invoice_tests 
 
 TEST(Invoice, DISABLED_No_Constraint_Applies_tax_rules_when_computing_total_amount) {
-	// Instantiate an Invoice sent to USA
-	// Add it a purchased novel costing 50
-	// Assert the total amount of the invoice is 56,35 : 15% of taxes plus a 2% reduction on novels
+	domain::country::Country usa("USA", domain::country::Currency::US_DOLLAR, domain::country::Language::ENGLISH);
+
+	domain::country::Country uk("UK", domain::country::Currency::POUND_STERLING, domain::country::Language::ENGLISH);
+	domain::book::Author georgeOrwell("George Orwell", uk);
+	std::vector<domain::book::Genre> genres = { domain::book::Genre::ADVENTURE_FICTION };
+	std::shared_ptr<domain::book::Book> novel1984(new domain::book::Novel("1984", 50., georgeOrwell, domain::country::Language::ENGLISH, genres));
+	auto purchased1984 = std::make_shared<purchase::PurchasedBook>(novel1984, 1);
+
+	purchase::Invoice invoice("Client A", usa);
+	invoice.addPurchasedBook(purchased1984);
 }
 
 TEST(Invoice, DISABLED_Test_Data_Builders_Constraint_Applies_tax_rules_when_computing_total_amount) {
