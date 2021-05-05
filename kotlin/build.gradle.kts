9c181a459e0a2d6314021e3c51f3fdd721f7a4a8
Ahmad Atwi <ahmadatwi23@gmail.com>
2021-05-05 20:49:13 +0200

    In order to provide a Kotlin version of the Workshop
    - Make gradle generate a jar file on build

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,4 +1,7 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    application
     kotlin("jvm") version "1.4.31"
 }
 
@@ -9,7 +12,12 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("com.murex.ApplicationKt")
+}
+
 dependencies {
+    implementation("commons-io:commons-io:2.6")
     implementation("com.google.code.gson:gson:2.8.6")
     testImplementation("org.assertj:assertj-core:3.19.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
@@ -21,7 +29,19 @@ tasks {
     "test"(Test::class) {
         useJUnitPlatform()
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+
+    withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
+    }
+
+    withType<Jar> {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes["Main-Class"] = application.mainClass }
+        dependsOn(configurations.runtimeClasspath)
+
+        from(configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) })
+
+        archiveFileName.set("MikadoTestBuilderKata.jar")
     }
 }
