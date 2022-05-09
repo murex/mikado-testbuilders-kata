913dc9f0b13e151454ed42f5997e0d48eb85e8d0
Yoan Thirion <yoan.thirion@gmail.com>
2022-05-09 11:37:44 +0200

    add murex Copyright

--- scala/src/main/scala/com/murex/tbw/storage/Repository.scala
+++ scala/src/main/scala/com/murex/tbw/storage/Repository.scala
@@ -1,3 +1,15 @@
+/*
+ * ******************************************************************************
+ *  *
+ *  * Copyright (c) {2003-2022} Murex S.A.S. and its affiliates.
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the MIT License
+ *  * which accompanies this distribution, and is available at
+ *  * https://opensource.org/licenses/MIT
+ *  *
+ *  ******************************************************************************
+ */
+
 package com.murex.tbw.storage
 
 import com.murex.tbw.purchase.Invoice
