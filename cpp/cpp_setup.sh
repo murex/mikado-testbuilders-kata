cd34f250171f497f85f55075d7bbe921fa7235a7
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-28 17:47:38 +0200

    Upgrade cmake to 3.26.2 in C++ setup scripts

--- cpp/cpp_setup.sh
+++ cpp/cpp_setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -eu
+
 case $(uname -s) in
 Darwin)
     os="macos"
@@ -20,8 +22,8 @@ Linux)
     cmake_generator_options="-G \"Unix Makefiles\""
     ;;
 MINGW64_NT-*)
-    os="win64"
-    arch="x64"
+    os="windows"
+    arch="x86_64"
     archive_extension="zip"
     cmake_bin_dir="bin"
     cmake="cmake.exe"
@@ -30,14 +32,14 @@ MINGW64_NT-*)
     ;;
 *)
     echo "os $(uname -s) is currently not supported."
-    exit
+    exit 1
     ;;
 esac
 
 base_dir=$(dirname -- "$0")
-cd "${base_dir}" || exit
+cd "${base_dir}"
 
-cmake_version="3.19.3"
+cmake_version="3.26.2"
 cmake_expected_dir="cmake-${cmake_version}-${os}-${arch}"
 cmake_expected_archive_file="${cmake_expected_dir}.${archive_extension}"
 cmake_archive_url="http://github.com/Kitware/CMake/releases/download/v${cmake_version}/${cmake_expected_archive_file}"
@@ -45,16 +47,18 @@ cmake_home="cmake-${os}-${arch}"
 
 build_dir="build"
 mkdir -p "${build_dir}"
-cd "${build_dir}" || exit
+cd "${build_dir}"
 
 cmake_build_dir="cmake"
 mkdir -p "${cmake_build_dir}"
-cd "${cmake_build_dir}" || exit
+cd "${cmake_build_dir}"
 
 if ! [ -f "${cmake_expected_archive_file}" ]
 then
+  echo "-- Downloading ${cmake_expected_archive_file}"
   curl -# -L "${cmake_archive_url}" -o "${cmake_expected_archive_file}"
 
+  echo "-- Extracting cmake ${cmake_version}"
   case "${archive_extension}" in
   zip)
     unzip -q -o "${cmake_expected_archive_file}"
@@ -64,7 +68,7 @@ then
     ;;
   *)
     echo "Archive format ${archive_extension} is currently not supported."
-    exit
+    exit 1
     ;;
   esac
 
