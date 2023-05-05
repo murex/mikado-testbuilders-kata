6197b05937bcd59ad573cbe9fd701cb31b9e1252
Ahmad Atwi <ahmadatwi23@gmail.com>
2023-05-05 09:49:47 +0200

    Upgrade Java & CMake (#84)
    * - Upgrade to Java 17
    - Update dependencies in gradle and maven
    - Used JUnit BOM
    - Used the newer version of Jar generation in gradle
    - Update the build commands in gradle and maven yaml files
    * Upgrade cmake to 3.26.2 in C++ setup scripts
    * Turn off gmock builld in C++
    
    ---------
    
    Co-authored-by: Ahmad ATWI <aatwi@murex.com>
    Co-authored-by: Damien Menanteau <damien.menanteau@gmail.com>

--- cpp/cmake/FindGoogleTest.cmake
+++ cpp/cmake/FindGoogleTest.cmake
@@ -4,5 +4,5 @@ CPMFindPackage(
   NAME googletest
   GITHUB_REPOSITORY google/googletest
   GIT_TAG main
-  OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt"
+  OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt" "BUILD_GMOCK OFF"
 )
