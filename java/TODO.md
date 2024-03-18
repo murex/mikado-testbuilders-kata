41665484d6985139d4245ce9b1156d850efdcf62
Philippe Bourgau <philippe.bourgau@gmail.com>
2024-03-18 16:42:51 +0100

    Create mikado for invoice test

--- java/TODO.md
+++ java/TODO.md
@@ -5,3 +5,8 @@ We will be using the mikado method to incrementally create the test data builder
 Indentation represents dependencies, and should be done first
 
 - [ ] Add a test to highlight the issue in Invoice, using the test data builder pattern
+  - [ ] Create a test data builder for the Invoice class
+    - [ ] Create a test data builder for the PurchaseBook class
+      - [ ] Create a test data builder for the Novel class
+        - [ ] Create test data builder for Author
+          - [ ] Create sample country data USA
--- java/src/test/java/com/murex/tbw/purchase/InvoiceTest.java
+++ java/src/test/java/com/murex/tbw/purchase/InvoiceTest.java
@@ -32,6 +32,13 @@ class InvoiceTest {
     void Mikado_Method_And_Test_Data_Builders_Constraint_Applies_tax_rules_when_computing_total_amount() {
         // Using the Mikado method and the Test Data Builder pattern:
         // Instantiate an Invoice sent to USA
+//        InvoiceBuilder.anInvoice()
+//                .sentTo("USA")
+//                .withItem(aNovel()
+//                        .costing(50))
+//                .build();
+
+
         // Add it a purchased novel costing 50
         // Assert the total amount of the invoice is 56,35 : 15% of taxes plus a 2% reduction on novels
     }
